6bc3da442b2feac7a1c40f0dcb71519e8163dfb1
Alex Lazcano <alazcano2000@gmail.com>
2022-07-14 00:49:30 -0700

    Added Form to Front end, organized files containding data. Resusable Forms

--- client/src/lib/constants.js
+++ client/src/lib/constants.js
@@ -1,1 +1,35 @@
 export const BACKEND_URL = process.env.REACT_APP_BACKEND
+
+export const TABLE_COLUMNS = {
+  students: {
+    TableName: 'Students',
+    Columns: [
+      {
+        title: 'SID',
+        dataIndex: 'SID',
+        key: 'sid',
+        hidden: true
+      },
+      {
+        title: 'First Name',
+        dataIndex: 'FirstName',
+        key: 'FirstName'
+      },
+      {
+        title: 'Last Name',
+        dataIndex: 'LastName',
+        key: 'LastName'
+      },
+      {
+        title: 'Birth Date',
+        dataIndex: 'BirthDate',
+        key: 'BirthDate'
+      },
+      {
+        title: 'Time Zone',
+        dataIndex: 'Timezone',
+        key: 'Timezone'
+      }
+    ]
+  }
+}
