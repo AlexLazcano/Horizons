2fb460411a02661a53ec84c4ab6004bfe3e31a13
sahbahh <sahba_hajihoseini@sfu.ca>
2022-07-22 23:41:44 -0700

    Added edit,delete, getID for multipleChoiceQ table

--- server/services/multipleChoiceQ.service.js
+++ server/services/multipleChoiceQ.service.js
@@ -27,9 +27,49 @@ const createMultipleChoiceQ = async multipleChoiceQ => {
     }
 }
 
+const deleteMultipleChoiceQ = async id => {
+    try {
+            const result = await db.query('DELETE FROM horizons.multiplechoiceq WHERE QID = ?', [id])
+        return {
+            message: result.affectedRows ? 'MultipleChoiceQ deleted' : 'MultipleChoiceQ not deleted'
+      }
+    } catch (error) {
+        console.log(error)
+    }
+  }
+  const updateMultipleChoiceQ = async (id, multipleChoiceQ) => {
+    try {
+        const { CorrectChoice, QuizID, Prompt, Choice1, Choice2, Choice3, Choice4 } = multipleChoiceQ
+        const result = await db.query(
+            'UPDATE horizons.multiplechoiceq SET CorrectChoice = ?, QuizID = ?, Prompt = ?, Choice1 = ?, Choice2 = ?, Choice3 = ?,Choice4 = ? WHERE QID = ?',
+            [CorrectChoice, QuizID, Prompt, Choice1, Choice2, Choice3, Choice4, id]
+        )
+      return {
+            message: result.affectedRows ? 'MultipleChoiceQ updated' : 'MultipleChoiceQ not updated',
+            status: result.affectedRows ? 200 : 400
+      }
+    } catch (error) {
+        console.log(error)
+    }
+  }
+  
+  const getQIDs = async () => {
+    try {
+        const rows = db.query('SELECT QID FROM horizons.multiplechoiceq')
+        return !rows ? [] : rows
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
 module.exports = {
     getMultipleChoiceQs,
-    createMultipleChoiceQ
+    createMultipleChoiceQ,
+    deleteMultipleChoiceQ,
+    updateMultipleChoiceQ,
+    getQIDs
+
+
 }
 
 
