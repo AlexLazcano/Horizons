269a4d1525cbc7d35e5e2875c52618be9fafb123
sha253 <sha253@sfu.ca>
2022-07-26 00:25:21 -0700

    Added InstructorsKnowLanguage request

---
+++ client/src/lib/requests/instructorsKnowLanguage/index.js
@@ -0,0 +1,81 @@
+import axios from 'axios'
+import { BACKEND_URL } from '../../constants'
+
+const instructorsKnowLanguageRequests = {
+  getAll: async () => {
+    const res = await axios
+      .get(`${BACKEND_URL}/instructorknowlanguage`)
+      .then(res => {
+        return res.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    return res
+  },
+  create: async ({ IID, LanguageCode }) => {
+    console.log('create', { IID, LanguageCode })
+    if (!IID || !LanguageCode) {
+      console.error('Missing required fields for InstructorKnowLanguage creation')
+    }
+    const res = await axios
+      .post(`${BACKEND_URL}/instructorknowlanguage`, {
+        IID,
+        LanguageCode
+      })
+      .then(res => {
+        console.log('post response', res)
+        return res
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    return res
+  },
+  delete: async (IID, LanguageCode) => {
+    console.log('delete', { IID, LanguageCode })
+    const res = await axios
+      .delete(`${BACKEND_URL}/instructorknowlanguage/${IID}/${LanguageCode}`)
+      .then(res => {
+        console.log('delete response', res)
+        return res
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+    return res
+  },
+  update: async (IID, LanguageCode, data) => {
+    const newLanguageCode = data.LanguageCode
+    console.log('update', { IID, LanguageCode, newLanguageCode })
+
+    const res = await axios
+      .patch(`${BACKEND_URL}/instructorknowlanguage/${IID}/${LanguageCode}`, {
+        newLanguageCode
+      })
+      .then(res => {
+        console.log('patch response', res)
+        return res
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+    return res
+  },
+  getTotalRows: async () => {
+    const res = await axios
+      .get(`${BACKEND_URL}/instructorknowlanguage/rows`)
+      .then(res => {
+        console.log('getTotalRows response', res)
+        return res.data[0].rowCount
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    return res
+  }
+}
+
+export default instructorsKnowLanguageRequests
